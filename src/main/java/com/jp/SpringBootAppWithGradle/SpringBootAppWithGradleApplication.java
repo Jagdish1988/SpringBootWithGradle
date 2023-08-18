6ed7edf4102ccea8c77a4a7a8cd426b145b0df09
Jagdish1988 <34127027+Jagdish1988@users.noreply.github.com>
2023-08-18 16:57:18 +0530

    Main method has changed.

--- src/main/java/com/jp/SpringBootAppWithGradle/SpringBootAppWithGradleApplication.java
+++ src/main/java/com/jp/SpringBootAppWithGradle/SpringBootAppWithGradleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootAppWithGradleApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello India.");
 		SpringApplication.run(SpringBootAppWithGradleApplication.class, args);
 	}
 
