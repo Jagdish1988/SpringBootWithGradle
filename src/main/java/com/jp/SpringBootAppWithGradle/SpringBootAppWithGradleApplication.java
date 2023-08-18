5c9437c58bf1da9f99801c6219d29ce33603f266
Jagdish1988 <34127027+Jagdish1988@users.noreply.github.com>
2023-08-18 16:25:08 +0530

    git commited the gradle project.

---
+++ src/main/java/com/jp/SpringBootAppWithGradle/SpringBootAppWithGradleApplication.java
@@ -0,0 +1,13 @@
+package com.jp.SpringBootAppWithGradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppWithGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppWithGradleApplication.class, args);
+	}
+
+}
